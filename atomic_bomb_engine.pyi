985d4eea5d555553ac59065c57599ccce72c583e
qyzhg <qyzhg@qyzhg.com>
2024-03-03 23:11:41 +0800

    feat: pyi更新

--- atomic_bomb_engine.pyi
+++ atomic_bomb_engine.pyi
@@ -13,22 +13,24 @@ def run(
                 form_data_str: str | None = None,
                 headers: list[str] | None = None,
                 cookie: str | None = None,
-                should_prevent:bool = False) -> dict:
-        """
-        同步启动压测引擎
-        :param url: 压测地址
-        :param method: 请求方式
-        :param test_duration_secs: 持续时间
-        :param concurrent_requests: 并发量
-        :param timeout_secs: 接口超时时间
-        :param verbose: 开启详情日志
-        :param json_str: 使用json请求发送请求,使用json字符串,不要使用字典类型
-        :param form_data_str: 使用form方式发送请求
-        :param headers: 添加请求头
-        :param cookie: 添加cookie
-        :param should_prevent: 实验性功能！压测过程中是否阻止休眠，此参数为true时，需要使用管理员权限运行才有效果，使用此功能会增加电脑功耗，但在无人值守时会非常有用
-        :return:
-        """
+                should_prevent:bool = False,
+                assert_json_path:str| None = None) -> dict:
+    """
+    同步启动压测引擎
+    :param url: 压测地址
+    :param method: 请求方式
+    :param test_duration_secs: 持续时间
+    :param concurrent_requests: 并发量
+    :param timeout_secs: 接口超时时间
+    :param verbose: 开启详情日志
+    :param json_str: 使用json请求发送请求,使用json字符串,不要使用字典类型
+    :param form_data_str: 使用form方式发送请求
+    :param headers: 添加请求头
+    :param cookie: 添加cookie
+    :param should_prevent: 实验性功能！压测过程中是否阻止休眠，此参数为true时，需要使用管理员权限运行才有效果，使用此功能会增加电脑功耗，但在无人值守时会非常有用
+    :param assert_json_path: 要提取值的jsonpath
+    :return:
+    """
 
 async def run_async(
         url: str,
@@ -41,7 +43,8 @@ async def run_async(
                 form_data_str: str | None = None,
                 headers: str | None = None,
                 cookie: str | None = None,
-                should_prevent:bool = False) -> dict:
+                should_prevent:bool = False,
+                assert_json_path:str| None = None) -> dict:
         """
         异步启动压测引擎
         :param url: 压测地址
@@ -55,6 +58,7 @@ async def run_async(
         :param headers: 添加请求头
         :param cookie: 添加cookie
         :param should_prevent: 实验性功能！压测过程中是否阻止休眠，此参数为true时，需要使用管理员权限运行才有效果，使用此功能会增加电脑功耗，但在无人值守时会非常有用
+        :param assert_json_path: 要提取值的jsonpath
         :return:
         """
 
