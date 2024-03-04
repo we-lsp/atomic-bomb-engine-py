c55d08e6eae38814464d6f078cd4499bd0dd9ad9
qyzhg <qyzhg@qyzhg.com>
2024-03-04 13:14:18 +0800

    fix： 构建脚本路径错误

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 maturin build --release --target x86_64-apple-darwin -i python3.8 -i python3.9 -i python3.10 -i python3.11 -i python3.12 &&
 maturin build --release -i python3.8 -i python3.9 -i python3.10 -i python3.11 -i python3.12 &&
 twine upload --repository pypi --config-file ~/.pypirc ./target/wheels/* &&
-rm ./tartet/wheels/*
+rm ./target/wheels/*
