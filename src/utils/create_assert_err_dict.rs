abd81ba81e416deb0c16ac5de26876cfe71b9ee9
qyzhg <qyzhg@qyzhg.com>
2024-03-04 20:27:28 +0800

    feat:更新错误数据返回值的结构

--- src/utils/create_assert_err_dict.rs
+++ src/utils/create_assert_err_dict.rs
@@ -1,16 +1,20 @@
 use std::collections::HashMap;
 use pyo3::{PyResult, Python};
 use pyo3::types::{PyDict, PyList};
-pub fn create_assert_error_dict<'py>(py: Python<'py>, assert_errors: &HashMap<(String, String), u32>) -> PyResult<&'py PyDict> {
+pub fn create_assert_error_dict<'py>(py: Python<'py>, assert_errors: &HashMap<(String, String), u32>) -> PyResult<&'py PyList> {
     if assert_errors.is_empty() {
-        return Ok(PyDict::new(py));
+        return Ok(PyList::empty(py));
     }
 
-    let assert_error_dict = PyDict::new(py);
+    let mut result_errors = Vec::new();
     for ((url, message), count) in assert_errors {
+        let assert_error_dict = PyDict::new(py);
+
         assert_error_dict.set_item("url", url)?;
         assert_error_dict.set_item("message", message)?;
         assert_error_dict.set_item("count", count)?;
+
+        result_errors.push(assert_error_dict)
     }
-    Ok(assert_error_dict)
+    Ok(PyList::new(py, result_errors))
 }
