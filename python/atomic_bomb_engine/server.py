5be0845ef4d96a803c064c8c102aca207fe8dbc5
qyzhg <qyzhg@qyzhg.com>
2024-03-12 14:36:12 +0800

    feat: server模式基本实现，等待前端链联调

--- python/atomic_bomb_engine/server.py
+++ python/atomic_bomb_engine/server.py
@@ -1,29 +1,49 @@
-from flask import Flask
-import asyncio
 import atomic_bomb_engine
+import os
+from aiohttp import web
+import asyncio
 
-def ui(f):
-    def wrapper(*args, **kwargs):
-        app = Flask(__name__)
-        res = ""
-        async def listen_batch():
-            iterator = atomic_bomb_engine.BatchListenIter()
-            for message in iterator:
-                if message:
-                    print(message)
-                else:
-                    await asyncio.sleep(0.3)
 
-        async def start():
-            nonlocal res
-            result = await asyncio.gather(f(), listen_batch())
-            res = result[0]
+def ui(func):
+    async def start_service(*args, **kwargs):
+        async def run_decorated_function(request):
+            result = await func(*args, **kwargs)
+            return web.json_response(result)
 
-        @app.route("/run")
-        def run():
-            asyncio.run(start())
-            return res
+        async def redirect_to_index(request):
+            raise web.HTTPFound('/static/index.html')
 
-        app.run()
-        return f(*args, **kwargs)
-    return wrapper
+        app = web.Application()
+        app.router.add_static('/static', path=os.path.join(os.path.dirname(__file__), 'dist'), name='dist')
+
+        app.add_routes([web.get('/', redirect_to_index),
+                        web.get('/ws', websocket_handler),
+                        web.get('/run', run_decorated_function)])
+
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, '0.0.0.0', 8000)
+        await site.start()
+
+        event = asyncio.Event()
+        await event.wait()
+    print("服务启动成功: http://localhost:8000")
+    return start_service
+
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    result_iter = atomic_bomb_engine.BatchListenIter()
+    await ws.prepare(request)
+    for item in result_iter:
+        if item:
+            await ws.send_json(item)
+        await asyncio.sleep(0.3)
+    try:
+        while True:
+            await asyncio.sleep(10)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await ws.close()
+    return ws
