57f2966763cc3f6ae9fe865c989062cc3a0a89bc
qyzhg <qyzhg@qyzhg.com>
2024-05-10 13:23:49 +0800

    feat: 基本实现

--- src/py_lib/batch_runner.rs
+++ src/py_lib/batch_runner.rs
@@ -4,8 +4,7 @@ use futures::stream::BoxStream;
 use futures::StreamExt;
 use crate::utils;
 use pyo3::types::{PyDict, PyList};
-use pyo3::{pyclass, pymethods, PyObject, PyRefMut, PyResult, Python, ToPyObject, PyAny};
-use pyo3_asyncio::tokio::future_into_py;
+use pyo3::{pyclass, pymethods, PyObject, PyRefMut, PyResult, Python, ToPyObject};
 use tokio::runtime::Runtime;
 use tokio::sync::Mutex;
 
@@ -75,12 +74,13 @@ impl BatchRunner {
             Ok::<(), pyo3::PyErr>(())
         };
 
-        // 将Rust future转换为Python future并直接返回
+        // 使用Tokio兼容的future_into_py
         Python::with_gil(|py| {
-            pyo3_asyncio::async_std::future_into_py(py, fut)
+            pyo3_asyncio::tokio::future_into_py(py, fut)
                 .map(|py_any| py_any.to_object(py)) // Convert &PyAny to Py<PyAny>
         })
     }
+
 
     fn __iter__(slf: PyRefMut<Self>) -> PyResult<PyRefMut<Self>> {
         Ok(slf)
@@ -101,6 +101,43 @@ impl BatchRunner {
             match next_stream {
                 Some(Ok(result)) => {
                     let dict = PyDict::new(py);
+                    if let Some(test_result) = result{
+                        dict.set_item("total_duration", test_result.total_duration)?;
+                        dict.set_item("success_rate", test_result.success_rate)?;
+                        dict.set_item("error_rate", test_result.error_rate)?;
+                        dict.set_item("median_response_time", test_result.median_response_time)?;
+                        dict.set_item("response_time_95", test_result.response_time_95)?;
+                        dict.set_item("response_time_99", test_result.response_time_99)?;
+                        dict.set_item("total_requests", test_result.total_requests)?;
+                        dict.set_item("rps", test_result.rps)?;
+                        dict.set_item("max_response_time", test_result.max_response_time)?;
+                        dict.set_item("min_response_time", test_result.min_response_time)?;
+                        dict.set_item("err_count", test_result.err_count)?;
+                        dict.set_item("total_data_kb", test_result.total_data_kb)?;
+                        dict.set_item(
+                            "throughput_per_second_kb",
+                            test_result.throughput_per_second_kb,
+                        )?;
+                        let http_error_list =
+                            utils::create_http_err_dict::create_http_error_dict(py, &test_result.http_errors)?;
+                        dict.set_item("http_errors", http_error_list)?;
+                        let assert_error_list = utils::create_assert_err_dict::create_assert_error_dict(
+                            py,
+                            &test_result.assert_errors,
+                        )?;
+                        dict.set_item("assert_errors", assert_error_list)?;
+                        dict.set_item("timestamp", test_result.timestamp)?;
+                        let api_results = utils::create_api_results_dict::create_api_results_dict(
+                            py,
+                            test_result.api_results,
+                        )?;
+                        dict.set_item("api_results", api_results)?;
+                        dict.set_item(
+                            "total_concurrent_number",
+                            test_result.total_concurrent_number,
+                        )?;
+                        dict.set_item("errors_per_second", test_result.errors_per_second)?;
+                    };
                     Ok(Some(dict.to_object(py)))
                 },
                 Some(Err(e)) => Err(pyo3::exceptions::PyRuntimeError::new_err(e.to_string())),
